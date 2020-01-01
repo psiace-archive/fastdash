8cc1f447589dc15282aa019f149e5595af9dc2fc
Chojan Shang <psiace@outlook.com>
2020-01-01 23:01:57 +0800

    :tada: Init skeleton.

---
+++ fastdash/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for FastDash."""
+
+__author__ = """Chojan Shang"""
+__email__ = "psiace@outlook.com"
+__version__ = "0.1.0"
---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Setup settings."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="fastdash",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
